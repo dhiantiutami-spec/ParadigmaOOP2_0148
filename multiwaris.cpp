004314c881ea7faa6e2edce9570d311744ad8148
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-05-19 18:34:37 +0700

    membuat class pelajar

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -21,3 +21,12 @@ public:
     }
 };
 
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
